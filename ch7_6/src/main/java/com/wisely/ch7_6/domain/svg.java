cc52b37e074febd0ac3481b95e4c241cc7e11181
wuyouxiang <552030698@qq.com>
2016-08-06 19:54:34 +0800

    Add files via upload

---
+++ ch7_6/src/main/java/com/wisely/ch7_6/domain/svg.java
@@ -0,0 +1,12 @@
+package com.wisely.ch7_6.domain;
+
+public class svg {
+	String points;
+	String id;
+	String svgid;
+	String opacity;
+	String strokelinecap;
+	String strokewidth;
+	String stroke;
+	String fill;
+}
